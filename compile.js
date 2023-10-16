02873c960429b24ce8ea5dd39297c0352d290997
juanarayafonsec <juan-jose95@hotmail.com>
2023-10-16 21:43:11 +0200

    Test of inbox contract

--- compile.js
+++ compile.js
@@ -5,10 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname,'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-const contract = solc.compile(source,1).contracts[':Inbox']
-const byteCode = contract.bytecode;
-const interfaceContract = contract.interface;
-//console.log({byteCode, interfaceContract});
-module.exports = {byteCode, interfaceContract}
-//console.log(solc.compile(source,1));
-//module.exports = solc.compile(source,1).contracts[':Inbox'];
+module.exports = solc.compile(source,1).contracts[':Inbox'];
